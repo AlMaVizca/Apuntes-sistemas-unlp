387da7790a3bd0561b3dd04235ad4c3b968fed01
krahser <aldo.vizcaino87@gmail.com>
2014-05-30 11:44:08 -0300

    compilado y funcionando

--- Paralelos/Prácticas/4/mpi.c
+++ Paralelos/Prácticas/4/mpi.c
@@ -9,84 +9,90 @@ double dwalltime();
 
 int j,n_proc,N,chunk;
 
-void first_work(){
-}
-
-
 int main(int argc,char*argv[]){
- double *A,*B;
- double min,max,avg_sum; 
+ double *A,*B, *Buffer;
+ double min,max,avg,avg_sum; 
  int i;
  int check=1;
  double timetick;
- double *buffer, *values;
-
  //Controla los argumentos al programa
   if (argc < 2){
    printf("\n Faltan argumentos:: N dimension de la matriz \n");
    return 0;
   }
-   N=atoi(argv[1]);
-
- //Aloca memoria para las matrices
-  A=(double*)malloc(sizeof(double)*N*N);
-  B=(double*)malloc(sizeof(double)*N*N);
-  buffer=(double*)malloc(sizeof(double)*chunk*N);
-  // Array to communicate min, max and average
-  values=(double*)malloc(sizeof(double)*3*N);
-
-
-  //Inicializa la matriz 
-  for(i=0;i<N*N;i++){
-	A[i]= 1.0;
-  }   
- 
-  int id;
- timetick = dwalltime();
-
+  N=atoi(argv[1]);
+ int worker_id, n_proc;
  MPI_Init(&argc, &argv);
  MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
- MPI_Comm_rank(MPI_COMM_WORLD, &id);
+ MPI_Comm_rank(MPI_COMM_WORLD, &worker_id);
  MPI_Status status;
- chunk=N/n_proc;
+ chunk=(N*N)/n_proc;
+ Buffer=(double*)malloc(sizeof(double)*chunk*N);
+ A=(double*)malloc(sizeof(double)*N*N);
+ B=(double*)malloc(sizeof(double)*N*N);
 
- MPI_Scatter (A, chunk*N, MPI_DOUBLE, buffer, chunk*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+ MPI_Scatter (A, chunk, MPI_DOUBLE, Buffer, chunk, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
- MPI_AllReduce (&buffer, &avg_sum, chunk*N, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
- MPI_AllReduce (&buffer, &values[0], chunk*N, MPI_DOUBLE, MPI_MIN, 0, MPI_COMM_WORLD);
- MPI_AllReduce (&buffer, &values[1], chunk*N, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  for(i=0;i<chunk;i++){
+	Buffer[i]= 1.0;
+  }   
  
- values[2]=avg_sum/N;
+ MPI_Gather(Buffer, chunk, MPI_DOUBLE, A, chunk, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+ timetick = dwalltime();
 
- MPI_Scatter (A, chunk*N, MPI_DOUBLE, buffer, chunk*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+ min=Buffer[1];
+ min=Buffer[1];
+ avg=0;
+  for(i=0;i<chunk;i++){
+    if (Buffer[i]<max) max=Buffer[i];
+    if (min<Buffer[i]) min=Buffer[i];
+    avg+=Buffer[i];
+  }   
+ 
+  MPI_Allreduce(&avg, &avg_sum, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Allreduce(&min, &min, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+  MPI_Allreduce(&max, &max, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+ 
+  avg=avg_sum/(N*N);
+ 
+ MPI_Scatter (A, chunk, MPI_DOUBLE, Buffer, chunk, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
- for(i=0;i<chunk*N;i++){
-    if (buffer[i]<avg) buffer[i]=min;
-    if (buffer[i]>avg) buffer[i]=max;
-    if (buffer[i]==avg) buffer[i]=avg;
+ for(i=0;i<chunk;i++){
+    if (Buffer[i]<avg) Buffer[i]=min;
+    if (Buffer[i]>avg) Buffer[i]=max;
+    if (Buffer[i]==avg) Buffer[i]=avg;
   }
 
 
- MPI_Gather(buffer, chunk*N, MPI_DOUBLE, B, chunk*N, chunk*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+ MPI_Gather(Buffer, chunk, MPI_DOUBLE, B, chunk, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 
 
- MPI_Finalize();
+
  printf("Tiempo en segundos: %f \n", dwalltime() - timetick);
 
   //Chequea los resultados
-  for(i=0;i<N*N;i++){
-	check= check&&(B[i]==1.0);
-  }   
 
+ MPI_Scatter (A, chunk, MPI_DOUBLE, Buffer, chunk, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+ for(i=0;i<chunk;i++){
+	check= check&&(Buffer[i]==1.0);
+  }
+
+  MPI_Allreduce(&check, &check, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+
+  if(worker_id == MANAGER){
   if(check){
-   printf("Resultado correcto\n");
+      printf("Resultado correcto");
   }else{
    printf("Resultado erroneo\n");
   }
+  }
  free(A);
  free(B);
+ free(Buffer);
+ MPI_Finalize();
  return(0);
 }
 
